7a0cd57f02a4a090975247815ffcee6b7fc893e9
Rick Morrison <rick@do106.priologic.com>
2015-03-23 16:37:24 -0400

    split encoding by transport, add program binary to ignores

--- main.go
+++ main.go
@@ -9,20 +9,74 @@ import (
 func main() {
 
 	dec := json.NewDecoder(os.Stdin)
-	enc := json.NewEncoder(os.Stdout)
 	for {
-		var v map[string]interface{}
-		if err := dec.Decode(&v); err != nil {
+		var kv map[string]interface{}
+		if err := dec.Decode(&kv); err != nil {
 			log.Println(err)
 			return
 		}
-		for k := range v {
-			if k != "Name" {
-				delete(v, k)
+		for k, v := range kv {
+			if k == "_TRANSPORT" {
+				switch v {
+				case "journal": encodeJournalRecord(kv)
+				case "kernel" : encodeKernelRecord(kv)
+				case "stdout": encodeStdoutRecord(kv)
+				case "syslog": encodeSyslogRecord(kv)
+				default: log.Printf("unexpected _TRANSPORT: %v\n", v)
+				}
 			}
 		}
-		if err := enc.Encode(&v); err != nil {
-			log.Println(err)
-		}
 	}
+}
+
+func encodeJournalRecord(v map[string]interface{}) {
+	m := make(map[string]interface{})
+	m["_TRANSPORT"] = "journal"
+	m["_HOSTNAME"] = v["_HOSTNAME"]
+	m["SYSLOG_IDENTIFIER"] = v["SYSLOG_IDENTIFIER"]
+	m["MESSAGE"] = v["MESSAGE"]
+	m["_SOURCE_REALTIME_TIMESTAMP"] = v["_SOURCE_REALTIME_TIMESTAMP"]
+        enc := json.NewEncoder(os.Stdout)
+        if err := enc.Encode(&m); err != nil {
+            log.Println(err)
+        }
+}
+
+func encodeKernelRecord(v map[string]interface{}) {
+        m := make(map[string]interface{})
+	m["_TRANSPORT"] = "kernel"
+        m["_HOSTNAME"] = v["_HOSTNAME"]
+        m["SYSLOG_IDENTIFIER"] = v["SYSLOG_IDENTIFIER"]
+        m["MESSAGE"] = v["MESSAGE"]
+        m["_SOURCE_REALTIME_TIMESTAMP"] = v["_SOURCE_REALTIME_TIMESTAMP"]
+        enc := json.NewEncoder(os.Stdout)
+        if err := enc.Encode(&m); err != nil {
+            log.Println(err)
+        }
+}
+
+func encodeStdoutRecord(v map[string]interface{}) {
+        m := make(map[string]interface{})
+	m["_TRANSPORT"] = "stdout"
+        m["_HOSTNAME"] = v["_HOSTNAME"]
+        m["SYSLOG_IDENTIFIER"] = v["SYSLOG_IDENTIFIER"]
+        m["MESSAGE"] = v["MESSAGE"]
+        m["_SOURCE_REALTIME_TIMESTAMP"] = v["_SOURCE_REALTIME_TIMESTAMP"]
+        enc := json.NewEncoder(os.Stdout)
+        if err := enc.Encode(&m); err != nil {
+            log.Println(err)
+        }
+}
+
+func encodeSyslogRecord(v map[string]interface{}) {
+        m := make(map[string]interface{})
+	m["_TRANSPORT"] = "syslog"
+        m["_HOSTNAME"] = v["_HOSTNAME"]
+        m["SYSLOG_IDENTIFIER"] = v["SYSLOG_IDENTIFIER"]
+        m["MESSAGE"] = v["MESSAGE"]
+        m["_SOURCE_REALTIME_TIMESTAMP"] = v["_SOURCE_REALTIME_TIMESTAMP"]
+        enc := json.NewEncoder(os.Stdout)
+        if err := enc.Encode(&m); err != nil {
+            log.Println(err)
+        }
 }
